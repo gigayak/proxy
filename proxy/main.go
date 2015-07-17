971b28d66a3dfd308b5900d1bd7dd4ee62c7c8b1
John Gilik <john@jgilik.com>
2015-07-16 21:41:30 -0400

    Add basic unauthenticated proxying within given domain.

--- proxy/main.go
+++ proxy/main.go
@@ -10,8 +10,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 var (
@@ -28,9 +31,53 @@ type proxyHandler struct {
 func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Security validation - prevents us from being an open proxy if this
 	// server is accidentally left exposed to the world.
-	//if (not subdomain of dir) fail
-	_ = r
-	http.Error(w, "not implemented", http.StatusInternalServerError)
+	if !strings.HasSuffix(r.URL.Host, "." + h.domain) && r.URL.Host != h.domain {
+		http.Error(w, "invalid host", http.StatusBadRequest)
+		return
+	}
+
+	// RequestURI cannot be set in requests, panics ensue otherwise.
+	r.RequestURI = ""
+
+	// Due to how Chrome handles HTTPS proxies, we will need to force HTTPS
+	// instead of expecting correct client behavior.
+	r.URL.Scheme = "https"
+	log.Printf("Proxying request for %q", r.URL.String())
+
+	// The following is straightforward proxying magic.
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		log.Printf("Error proxying request for %q: %v", r.URL.String(), err)
+		http.Error(w, fmt.Sprintf("error making proxied request: %v", err),
+			http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Got response for %q", r.URL.String())
+	for k, values := range resp.Header {
+		w.Header()[k] = values
+	}
+	w.WriteHeader(resp.StatusCode)
+	for {
+		buf := make([]byte, 1024)
+		n, err := resp.Body.Read(buf)
+		eof := err == io.EOF
+		if err != nil && !eof {
+			http.Error(w, fmt.Sprintf("error reading body: %v", err),
+				http.StatusInternalServerError)
+		}
+		written, err := w.Write(buf[:n])
+		if err != nil {
+			log.Printf("Failed to write response: %v", err)
+			return
+		}
+		log.Printf("Wrote %d response bytes", written)
+		if eof || n == 0 {
+			break
+		}
+	}
+	if err := resp.Body.Close(); err != nil {
+		log.Printf("Error closing connection: %v", err)
+	}
 }
 
 func main() {
