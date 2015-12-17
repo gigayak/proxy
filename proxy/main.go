566b26faff9a7a051896b7cf7f4449f58c08640a
John Gilik <john@jgilik.com>
2015-12-16 17:51:11 -0800

    Go is not Python.  Go is not Python.

--- proxy/main.go
+++ proxy/main.go
@@ -127,7 +127,7 @@ func main() {
 
 	pool := x509.NewCertPool()
 	var foundCA bool
-	for _, path in range strings.Split(*cacerts) {
+	for _, path := range strings.Split(*cacerts) {
 		b, err := ioutil.ReadFile(path)
 		if err != nil {
 			log.Printf("Failed to read client CA PEM file %q", path)
